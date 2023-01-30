081f2ec2e99b21fa41e0d1e2dc4d74e9d6582957
SharonJenn <123975176+SharonJenn@users.noreply.github.com>
2023-01-30 23:05:54 +0200

    my first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+
+temperature = float(input("Enter temperature in Celsius: "))
+
+print("Temperature in Fahrenheit: ", celsius_to_fahrenheit(temperature))
+print("Temperature in Celsius: ", fahrenheit_to_celsius(celsius_to_fahrenheit(temperature)))
